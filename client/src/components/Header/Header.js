3a4d41648369fb3c2d863ac23d1cabdc6c2273d6
Brian Lin <brian1999lin@gmail.com>
2021-03-09 00:50:21 -0500

    Add scss compiler, generate-react-cli files

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.scss';
+
+import logo from '../../logo.svg';
+
+const Header = () => (
+  <header className="Header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+);
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
